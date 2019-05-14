34ed093d918167817977d6eba27eef246b51c0df
Brian Fink <bfink@indeed.com>
2019-05-14 09:14:27 -0500

    #13 Add an additional factory method for providing a custom OkHttpClient.Builder

--- src/main/java/com/indeed/rabbitmq/admin/RabbitManagementApiFactory.java
+++ src/main/java/com/indeed/rabbitmq/admin/RabbitManagementApiFactory.java
@@ -18,31 +18,49 @@ public class RabbitManagementApiFactory {
     /**
      * Construct and return a new {@code RabbitManagementApi} instance.
      *
-     * @param uri the URI to the {@code RabbitMQ} service.
+     * @param uri the {@code java.net.URI} representing the base address of the RabbitMQ instance to talk to.
      * @param username the name of the administrator.
      * @param password the administrator's password.
      * @return an instance of the {@code RabbitManagementApi} interface.
      */
     public static RabbitManagementApi newInstance(final URI uri, final String username, final String password) {
+        return newInstance(new OkHttpClient.Builder(), uri, username, password);
+    }
+
+    /**
+     * Construct and return a new {@code RabbitManagementApi} instance with no authentication.
+     *
+     * @param uri the {@code java.net.URI} representing the base address of the RabbitMQ instance to talk to.
+     * @return an instance of the {@code RabbitManagementApi} interface.
+     */
+    public static RabbitManagementApi newInstance(final URI uri) {
+        return newInstance(new OkHttpClient.Builder(), uri, null, null);
+    }
+
+    /**
+     * Construct and return a new {@code RabbitManagementApi} instance using the provided {@code okhttp3.OkHttpClient.Builder}.
+     *
+     * @param okHttpClientBuilder The client-configured {@code okhttp3.OkHttpClient.Builder}. An additional interceptor will be automatically
+     *                            added for authentication if username and password are non-null.
+     * @param uri the {@code java.net.URI} representing the base address of the RabbitMQ instance to talk to.
+     * @param username the name of the administrator.
+     * @param password the administrator's password.
+     * @return an instance of the {@code RabbitManagementApi} interface.
+     */
+    public static RabbitManagementApi newInstance(final OkHttpClient.Builder okHttpClientBuilder, final URI uri, final String username, final String password) {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        final OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
-
         if ((null != username) && (null != password)) {
-            okHttpBuilder.addInterceptor(new AuthenticationInterceptor(username, password));
+            okHttpClientBuilder.addInterceptor(new AuthenticationInterceptor(username, password));
         }
 
         return new Retrofit.Builder()
                 .addConverterFactory(JacksonConverterFactory.create(objectMapper))
                 .baseUrl(uri.toASCIIString())
-                .client(okHttpBuilder.build())
+                .client(okHttpClientBuilder.build())
                 .build()
                 .create(RabbitManagementApi.class);
-    }
-
-    public static RabbitManagementApi newInstance(final URI uri) {
-        return newInstance(uri, null, null);
     }
 
     private RabbitManagementApiFactory() {
